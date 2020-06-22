7ddf39ff444c7ea2422ced34269faaf480fcc2af
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2020-06-23 02:20:39 +0530

    Add files via upload

--- frontend/form.js
+++ frontend/form.js
@@ -1,11 +1,17 @@
 
 function form()
 {
+    
+    //refer the docs and then write the object
     var data={
-        name:document.getElementById('firstName').value+document.getElementById('lastName').value,
-        company:document.getElementById('companyName').value,
-        github:document.getElementById('githuburl').value,
-        linkedin:document.getElementById('linkedinurl').value
+        "firstName": document.getElementById('firstName').value,
+        "lastName": document.getElementById('lastName').value,
+        "companyName": document.getElementById('companyName').value,
+        "githubUrl": document.getElementById('githuburl').value,
+        "linkedinUrl": document.getElementById('linkedinurl').value,
+        
+         
+       
     }
     var xh = new XMLHttpRequest();
     xh.open("POST", "https://business-card-api-rs.herokuapp.com/api/users/add", true)
@@ -23,3 +29,28 @@ function form()
         }
 }
 }
+
+function getall()
+{
+  
+    var xh = new XMLHttpRequest();
+    xh.open("GET", "https://business-card-api-rs.herokuapp.com/api/users/one", true)
+    xh.setRequestHeader('Content-Type', 'application/json')
+    xh.setRequestHeader('userId', "5ee6e773a49a9b0017affa94")
+    xh.send()
+    xh.onload=function(){
+        if(this.status==200)
+        {
+            var data = JSON.parse(this.responseText)
+
+            
+        }
+        else {
+            alert('Error in getting items')
+        }
+        
+}
+}
+
+
+
