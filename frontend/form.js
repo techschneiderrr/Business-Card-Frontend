bc781f1eac500b6fc4e10256c5324b39f53fc895
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2020-07-10 18:52:39 +0530

    Add files via upload

--- frontend/form.js
+++ frontend/form.js
@@ -17,40 +17,37 @@ function form()
     xh.open("POST", "https://business-card-api-rs.herokuapp.com/api/users/add", true)
     xh.setRequestHeader('Content-Type', 'application/json')
     xh.send(JSON.stringify(data))
-    xh.onload=function(){
+    xh.onload=function()
+    {
         if(this.status==200)
         {
             alert('request successful !')
             window.location.replace('display.html')
         }
-        else{
+        else
+        {
             alert('Failed! Try again')
             window.location.replace('form1.html')
         }
-}
+        
+    }
 }
 
-function getall()
+function getDetails()
 {
-  
     var xh = new XMLHttpRequest();
-    xh.open("GET", "https://business-card-api-rs.herokuapp.com/api/users/one", true)
+    xh.open("GET","https://business-card-api-rs.herokuapp.com/api/users/one",true)
+    
+    xhr.responseType = 'json'
     xh.setRequestHeader('Content-Type', 'application/json')
-    xh.setRequestHeader('userId', "5ee6e773a49a9b0017affa94")
     xh.send()
-    xh.onload=function(){
+    xh.onload=function()
+    {
         if(this.status==200)
         {
             var data = JSON.parse(this.responseText)
 
-            
+          console.log(data)
         }
-        else {
-            alert('Error in getting items')
-        }
-        
+    }
 }
-}
-
-
-
